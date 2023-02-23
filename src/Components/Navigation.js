e0837d4bad4df0fcca7d2d16e499dcf60f5d24d3
Juan Pablo Zavala <jpacc0405@gmail.com>
2023-02-23 08:37:15 -0800

    Fixed links

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -7,7 +7,10 @@ function Navigation() {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link href="">Github</Nav.Link>
+            <Nav.Link href="mailto: zavalajp@uci.edu">
+              zavalajp@uci.edu
+            </Nav.Link>
+            <Nav.Link href="https://github.com/jpzavala">Github</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
